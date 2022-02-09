8cc26f6ba0187b368fc5807bf8dba033ef313b99
Johwanyeong <85853166+Johwanyeong@users.noreply.github.com>
2022-02-09 11:48:24 +0900

    20220209
    계약

--- src/main/java/com/example/controller/ContractController.java
+++ src/main/java/com/example/controller/ContractController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -40,25 +41,28 @@ public class ContractController {
     PlayerService pService;
 
     //계약하기
-    // 127.0.0.1:8080/REST/contractinsert
+    // 127.0.0.1:8080/REST/contractinsert?sno=
     @RequestMapping(value = "/contractinsert", method = {RequestMethod.POST},
     consumes = MediaType.ALL_VALUE,
     produces = MediaType.APPLICATION_JSON_VALUE)
     public Map<String, Object> contractinsertPOST(
-        @RequestBody Contract contract,
         @ModelAttribute Player player,
+        @ModelAttribute Contract contract,
+        @RequestParam(name = "sno", defaultValue = "0")long scoutNo,
         @RequestHeader("token") String token){
         Map<String, Object> map = new HashMap<String, Object>();
         try{
             String memberid = jwtUtil.extractUsername(token); // 토큰을 통해 회원 정보 찾기
-            Long sno = contract.getScout().getScoutno(); // scout 정보 찾기
-            Scout scout = sService.getScoutOne(sno);
-            String scoutId = sService.getScoutOne(sno).getMember().getUserid(); // scout에서 member 정보 찾기
-            
+            Scout scout = sService.getScoutOne(scoutNo);
+            String scoutId = scout.getMember().getUserid(); // scout에서 member 정보 찾기
             if(memberid.equals(scoutId)){ // 로그인 memberid와 scout에서 찾은 memberid가 일치하는지 확인
-                contract.setScout(scout);
-                cService.insertContract(contract);
+                Player player2 = scout.getPlayer(); // scout에서 선수 정보 찾기
+                player2.setPlayerprice(player.getPlayerprice());    //몸값
+                player2.setTeam(player.getTeam()); //소속팀
+                pService.updatePlayer(player2);
                 System.out.println("----------------------------");
+                System.out.println(player.getPlayerprice());
+                System.out.println(contract.getScout().getPlayer().getPlayername());
                 System.out.println(contract.getScout().getScoutno());
                 System.out.println("----------------------------");
                 map.put("status", 1);
@@ -66,17 +70,9 @@ public class ContractController {
             else{
                 map.put("status", 2);
             }
-            if(contract.getScout().getScoutno() != null){
-                Player player2 = sService.getScoutOne(sno).getPlayer(); // scout에서 선수 정보 찾기
-                //계약이 성공하면 해당 player의 팀, 몸값 정보를 수정한다.
-                player2.setPlayerprice(player.getPlayerprice());    //몸값
-                pService.updatePlayer(player2);
-                System.out.println("----------------------------");
-                System.out.println(player.getPlayerprice());
-                System.out.println(contract.getScout().getPlayer().getPlayername());
-                
-                System.out.println(contract.getScout().getScoutno());
-                System.out.println("----------------------------");
+            if(player.getPlayerprice() != null){ //계약이 성공하면 해당 player의 팀, 몸 값 정보를 수정한다.
+                contract.setScout(scout);
+                cService.insertContract(contract);
                 // 계약이 성공하면 해당 scout의 player 정보를 null로 수정한다.
                 scout.setMember(null);
                 sService.updateScout(scout);
@@ -84,8 +80,7 @@ public class ContractController {
             }
             else{
                 map.put("status", 300);
-            }
-            
+            } 
         }
         catch(Exception e){
             e.printStackTrace();
@@ -93,5 +88,4 @@ public class ContractController {
         }
         return map;
     }
-
 }
