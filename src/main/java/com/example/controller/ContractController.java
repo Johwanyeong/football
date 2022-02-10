6a58c1676e2da19b01c94ec4360fb95de9a451f6
Johwanyeong <85853166+Johwanyeong@users.noreply.github.com>
2022-02-10 17:12:57 +0900

    20220210
    .

--- src/main/java/com/example/controller/ContractController.java
+++ src/main/java/com/example/controller/ContractController.java
@@ -15,7 +15,6 @@ import com.example.service.ScoutService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
